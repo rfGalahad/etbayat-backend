03ee195e5f5f12805bfb9141bca2bfdbb084a5c6
Ruther Solloso <rfsolloso03@gmail.com>
2025-07-12 22:11:54 +0800

    July 12, 2025 10:11PM

--- config/database.js
+++ config/database.js
@@ -5,10 +5,7 @@ const pool = mysql.createPool({
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  ssl: {
-    rejectUnauthorized: false // Required by Render
-  }
+  database: process.env.DB_DATABASE
 });
 
 // Test connection on startup
--- server.js
+++ server.js
@@ -17,14 +17,14 @@ import duplicateRoutes from './routes/duplicateRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+// app.use(cors());
 
-/*
+
 app.use(cors({
   origin: 'https://e-tbayatmswdo.com',
   credentials: true
 }));
-*/
+
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
